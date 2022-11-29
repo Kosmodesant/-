b8752f5920d07e2dacd894b4b0a6229f1ab022e9
Kosmodesant <90253666+Kosmodesant@users.noreply.github.com>
2022-11-29 12:57:59 +0300

    Create BubleSort.cpp

---
+++ BubleSort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main() 
+{
+  int arr[] = { 10, 3, 4, 1, 8, 9, 7, 2, 6, 5 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	bool swapped = false;
+
+	std::cout << "Массив до сортировки: ";
+
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+
+	std::cout << std::endl;
+
+	do
+	{
+		swapped = false;
+		for (int i = 1; i < size; i++)
+		{
+			if (arr[i - 1] > arr[i])
+			{
+				int temp = arr[i - 1];
+				arr[i - 1] = arr[i];
+				arr[i] = temp;
+				swapped = true;
+			}
+		}
+	} while (swapped);
+
+	std::cout << "Массив после сортировки: ";
+
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+
+	std::cout << std::endl;
+} 
