4fe7cd49581a378026d929d462fb571e6435a22d
Kosmodesant <90253666+Kosmodesant@users.noreply.github.com>
2022-12-04 19:03:32 +0300

    Update BubleSort.cpp

--- BubleSort.cpp
+++ BubleSort.cpp
@@ -39,3 +39,49 @@ int main()
 
 	std::cout << std::endl;
 } 
+
+
+
+
+// #include <iostream>
+
+// int main() 
+// {
+//   int arr[] = { 10, 3, 4, 1, 8, 9, 7, 2, 6, 5 };
+// 	int size = sizeof(arr) / sizeof(arr[0]);
+// 	bool swapped = false;
+
+// 	std::cout << "Массив до сортировки: ";
+
+// 	for (int i = 0; i < size; i++)
+// 	{
+// 		std::cout << arr[i] << " ";
+// 	}
+
+// 	std::cout << std::endl;
+
+// 	do
+// 	{
+// 		swapped = false;
+// 		for (int i = size -1; i > 0; i--)
+// 		{
+// 			if (arr[i - 1] > arr[i]) // при смене знака сортировка от большего к меньшему
+// 			{
+// 				int temp = arr[i - 1];
+// 				arr[i - 1] = arr[i];
+// 				arr[i] = temp;
+// 				swapped = true;
+// 			}
+// 		}
+// 	} while (swapped);
+
+// 	std::cout << "Массив после сортировки: ";
+
+// 	for (int i = 0; i < size; i++)
+// 	{
+// 		std::cout << arr[i] << " ";
+// 	}
+
+// 	std::cout << std::endl;
+// } 
+
