3ad05c58527a48c74df77a163ee0ae78c3bbc918
Kosmodesant <90253666+Kosmodesant@users.noreply.github.com>
2023-03-09 21:18:59 +0300

    Update BubleSort.cpp

--- BubleSort.cpp
+++ BubleSort.cpp
@@ -85,3 +85,59 @@ int main()
 // 	std::cout << std::endl;
 // } 
 
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     setlocale(LC_ALL, "Russian");
+
+//     int arr[] = { 10, 3, 4, 1, 8, 9, 7, 2, 6, 5, 8, 23 };
+//     int size = sizeof(arr) / sizeof(arr[0]);
+
+//     cout << "Массив до сортировки: ";
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+
+//     cout << endl;
+
+//     for (int i = 0; i < size - 1; i++) {
+//         for (int j = size - 1; j > i; j--) {
+//             if (arr[j] > arr[j - 1]) {
+//                 // меняем элементы местами
+//                 int temp = arr[j];
+//                 arr[j] = arr[j - 1];
+//                 arr[j - 1] = temp;
+//             }
+//         }
+//     }
+
+
+//     //for (int i = 0; i < size - 1; i++) {
+//     //    for (int j = 0; j < size - i - 1; j++) {
+//     //        if (arr[j] > arr[j + 1]) {
+//     //            // меняем элементы местами
+//     //            int temp = arr[j];
+//     //            arr[j] = arr[j + 1];
+//     //            arr[j + 1] = temp;
+//     //        }
+//     //    }
+//     //}
+
+
+//     cout << "Массив после сортировки: ";
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+
+//     cout << endl;
+// }
+
+
